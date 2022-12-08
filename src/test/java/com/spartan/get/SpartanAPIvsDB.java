113723b30c9ecf81aa423d21b20c385cd10f381f
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-08 21:53:41 +0000

    SpartanAPIvsDB.java

---
+++ src/test/java/com/spartan/get/SpartanAPIvsDB.java
@@ -0,0 +1,48 @@
+package com.spartan.get;
+
+import com.utilities.DBUtils;
+import com.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanAPIvsDB extends SpartanTestBase {
+
+    @DisplayName("GET one spartan from api and database")
+    @Test
+    public void testDB1() {
+        //1. get id,name,gender phone from database
+        String query = "select spartan_id,name,gender,phone from spartans\n" +
+                "where spartan_id = 15";
+
+        Map<String, Object> dbMap = DBUtils.getRowMap(query);
+        System.out.println("dbMap = " + dbMap);
+
+        //2.get info from api
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id", 15)
+        .when()
+                .get("/api/spartans/{id}")
+        .then()
+                .statusCode(200)
+                .and().contentType("application/json")
+                .extract().response();
+
+        Map<String,Object> apiMap= response.as(Map.class);
+        System.out.println(apiMap);
+
+        //3.compare database vs api --> we assume expected result is db
+        assertThat(apiMap.get("id").toString(),is(dbMap.get("SPARTAN_ID").toString()));
+        assertThat(apiMap.get("name"),is(dbMap.get("NAME")));
+        assertThat(apiMap.get("gender"),is(dbMap.get("GENDER")));
+        assertThat(apiMap.get("phone").toString(),is(dbMap.get("PHONE").toString()));
+
+    }
+}
