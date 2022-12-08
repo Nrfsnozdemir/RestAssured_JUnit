a4efa491e10fc89f076808777d1dca847d741368
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-08 21:53:25 +0000

    SpartanTestBase.java updated for DB connection

--- src/test/java/com/utilities/SpartanTestBase.java
+++ src/test/java/com/utilities/SpartanTestBase.java
@@ -1,5 +1,6 @@
 package com.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -9,5 +10,16 @@ public abstract class SpartanTestBase {
     @BeforeAll
     public static void init() {
         baseURI = ConfigurationReader.getProperty("spartanBaseURI");
+
+        String dbUrl = ConfigurationReader.getProperty("dbUrl");
+        String dbUsername = ConfigurationReader.getProperty("dbUsername");
+        String dbPassword = ConfigurationReader.getProperty("dbPassword");
+
+        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        DBUtils.destroy();
     }
 }
