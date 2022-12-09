8c4142784724bc31ba860cf549c1c0a064a25db1
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-09 13:53:23 +0000

    Link.java

---
+++ src/test/java/com/hr/pojo/Link.java
@@ -0,0 +1,16 @@
+package com.hr.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.junit.jupiter.api.DisplayName;
+
+@Getter
+@Setter
+@ToString
+
+public class Link {
+
+    private String rel;
+    private String href;
+}
